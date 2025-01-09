3af0d71fb3050a8e2ca522b11035680b714a0daa
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-01-08 23:41:19 -0300

    feat: add new migration.

---
+++ src/Data/Migrations/20250109023941_addRoleUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catedra3IDWMBackend.src.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addRoleUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c7afcb2f-5ba5-4f46-a98b-08c30f75fd31", null, "User", "USER" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7afcb2f-5ba5-4f46-a98b-08c30f75fd31");
+        }
+    }
+}
